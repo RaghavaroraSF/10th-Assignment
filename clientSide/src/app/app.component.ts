3300535a09af5619df44b9b48519441819c6d401
RaghavaroraSF <97958393+RaghavaroraSF@users.noreply.github.com>
2022-05-04 13:32:50 +0530

    updated

--- clientSide/src/app/app.component.ts
+++ clientSide/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Assignment 10';
+  title = '10th-Assignment';
   showTable:boolean = false;
 
   onClick()
